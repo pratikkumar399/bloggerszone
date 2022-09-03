f3f8e0f08ce69a6fc0d5094c5f91c1d977b250c0
Pratik <pratikkumar399@gmail.com>
2022-09-04 03:51:42 +0530

    complete blog website

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import React from 'react';
+
+import '../styles/globals.scss';
+import { Layout } from '../components';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
-export default MyApp
+export default MyApp;
+
+;
--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,33 @@
-import Head from 'next/head'
+import { FeaturedPosts } from '../sections/index';
+import { PostCard, Categories, PostWidget } from '../components';
+import { getPosts } from '../services';
 
-const posts = [
-  {
-    title: 'Learning React',
-    excerpt: 'Learn react testing'
-  },
-  {
-    title: 'Learning React with tailwind',
-    excerpt: 'Learn react testing with tailwindCSS'
-
-  },
-];
-export default function Home() {
+export default function Home({ posts }) {
   return (
     <div className="container mx-auto px-10 mb-8">
-      <Head>
-        <title>Bloggers Pro</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
-        {
-          posts.map((post, index) => (
-            <div>
-              {post.title}
-              {post.excerpt}
-            </div>
-          )
-          )}
+      <FeaturedPosts />
+      <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+        <div className="lg:col-span-8 col-span-1">
+          {posts.map((post, index) => (
+            <PostCard key={index} post={post.node} />
+          ))}
+        </div>
+        <div className="lg:col-span-4 col-span-1">
+          <div className="lg:sticky relative top-8">
+            <PostWidget />
+            <Categories />
+          </div>
+        </div>
       </div>
     </div>
-  )
+  );
 }
+
+// Fetch data at build time
+export async function getStaticProps() {
+  const posts = (await getPosts()) || [];
+  return {
+    props: { posts },
+  };
+}
+
